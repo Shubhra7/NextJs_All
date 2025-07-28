7da93c3a5e9490bd447bb6ad527d41c6c1effbe0
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-07-29 00:34:39 +0530

    Add About page and update layout metadata title

---
+++ blog-hunt/src/app/about/page.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "About Us",
+  description: "Learn more about our team and mission.",
+};
+
+export default function AboutPage () {
+  return (
+    <>
+      <h1>About hello</h1>
+    </>
+  );
+}
