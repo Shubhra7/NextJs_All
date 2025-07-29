6976558110a877db1f869fd331ebb4ddd4d2b135
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-07-30 00:28:03 +0530

    Add About page styling with styled-jsx and create Counter component

--- blog-hunt/src/app/about/page.js
+++ blog-hunt/src/app/about/page.js
@@ -1,16 +1,31 @@
+'use client';
 import Script from "next/script";
 import styles from './About.module.css';
 
-export const metadata = {
-  title: "About Us",
-  description: "Learn more about our team and mission.",
-};
+
+// 'use client' made it client side rendering, but metadata make it server side rendering
+// so have to comment any one
+
+// export const metadata = {
+//   title: "About Us",
+//   description: "Learn more about our team and mission.",
+// };
 
 export default function AboutPage () {
   return (
     <>
     <div className={styles.container}>
       <h1 className={styles.heading}>About hello</h1>
+      <h2>Testing the styled jsx</h2>
+      <style jsx>
+        {`
+          h2{
+            color: #424242ff
+          }
+        `}
+      </style>
+
+
     </div>
 
       <Script src="/sj.js" strategy="beforeInteractive"></Script>
